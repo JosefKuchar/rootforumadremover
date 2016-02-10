ff1e6db2a63821b132c96588b51193c3977ddd40
Josef Kuchař <josef.kuchar267@gmail.com>
2016-02-10 11:29:59 +0100

    New forum on root.cz

--- Root forum ad element blocker.user.js
+++ Root forum ad element blocker.user.js
@@ -1,10 +1,12 @@
 // ==UserScript==
 // @name         Root forum ad element blocker
 // @namespace    http://www.josefkuchar.cz/
-// @version      1.0.0
+// @version      2.0.0
 // @description  Remove ad elements in Root.cz forum discussions
 // @author       Josef Kuchař
 // @match        http://forum.root.cz/*
 // @grant        none
 // ==/UserScript==
-$(".windowbg2 .poster a[href='http://www.root.cz/']").parent().parent().parent().remove();
+var element = $(".body_message--ad");
+element.next().remove();
+element.remove();
